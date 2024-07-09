08835efe450cb944159641aacf09cc7f74afc9e3
Rúben Dias <110482980+aspaceusername@users.noreply.github.com>
2024-07-09 18:25:00 +0100

    About Page

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CPTWorkouts.Models;
+
+namespace CPTWorkouts.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new AboutViewModel
+            {
+            };
+
+            return View(model);
+        }
+    }
+}
